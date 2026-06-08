82523816c391ebc54837f316b5da192ce668ea8f
kimijimasan-lgtm <283345349+kimijimasan-lgtm@users.noreply.github.com>
2026-06-08 20:21:19 +0900

    save

---
+++ .tiptap-build/entry.js
@@ -0,0 +1,6 @@
+export { Editor } from '@tiptap/core';
+export { default as StarterKit } from '@tiptap/starter-kit';
+export { default as ImageExtension } from '@tiptap/extension-image';
+export { default as YoutubeExtension } from '@tiptap/extension-youtube';
+export { default as TaskList } from '@tiptap/extension-task-list';
+export { default as TaskItem } from '@tiptap/extension-task-item';
