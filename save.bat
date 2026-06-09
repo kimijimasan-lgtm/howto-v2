d2a2c988ed6c940fba81c15239550331cb723b52
kimijimasan-lgtm <283345349+kimijimasan-lgtm@users.noreply.github.com>
2026-06-09 20:46:12 +0900

    auto save

--- save.bat
+++ save.bat
@@ -1,12 +1,6 @@
 @echo off
-cd /d "C:\Users\kimij\OneDrive\ƒhƒLƒ…ƒƒ“ƒg\00_ClaudeCode\howto-v2"
+cd /d "C:\Users\kimij\OneDrive\ãƒ‰ã‚­ãƒ¥ãƒ¡ãƒ³ãƒˆ\00_ClaudeCode\howto-v2"
 git add .
-git commit -m "save"
+git commit -m "auto save"
 git push
-if errorlevel 1 goto error
-echo Done.
-goto end
-:error
-echo Failed.
-:end
 pause
